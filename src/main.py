7d480a120ff38c2edda5ec265ded9e5918bf8de4
AlexSaba <60479124+AlexSaba@users.noreply.github.com>
2026-07-07 10:51:35 +0200

    geringfuegige Aenderungen an der Fassade

--- src/main.py
+++ src/main.py
@@ -15,15 +15,15 @@ frm.grid()
 
 # Label mit Referenz speichern, damit wir es später aktualisieren können
 label_pfad = ttk.Label(frm, text="Datei-Pfad: (keine Datei gewählt)")
-label_pfad.grid(column=0, row=2, columnspan=10, sticky=W)
+label_pfad.grid(column=0, row=2, columnspan=5, sticky=W)
 
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=10)
 ttk.Button(frm, text="Datei öffnen", command=lambda: textFileReader(label_pfad, frm)).grid(column=0, row=1)
 ttk.Button(frm, text="Wörter zählen", command=lambda: countWords(tree)).grid(column=2, row=10)
 
 # Treeview
-tree = ttk.Treeview(root, column=("c1", "c2"), show='headings', height=15)
-#, x=530, y=60, height=100
+tree = ttk.Treeview(root, columns=("c1", "c2"), show='headings', height=20)
+tree.place(anchor='center', relx=0.5, rely=0.5, relwidth=0.9, relheight=0.7)
 
 # Scrollbar
 vsb = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
@@ -35,6 +35,6 @@ tree.column("# 1", anchor=CENTER, width=150)
 tree.heading("# 1", text="Häufigkeit")
 tree.column("# 2", anchor=CENTER, width=150)
 tree.heading("# 2", text="Wort")
-tree.grid(row=2, column=5, columnspan=7, padx=30, pady=30)
+tree.grid(row=5, column=0, columnspan=5, padx=5, pady=5)
 
 root.mainloop()
